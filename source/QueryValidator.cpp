432bae1a238667b1d3129cd5d1e2ebebb4745396
Si Ling <siling1992@gmail.com>
2015-01-21 20:41:41 +0800

    Moving code towards table-driven approach

--- source/QueryValidator.cpp
+++ source/QueryValidator.cpp
@@ -3,13 +3,38 @@
 #pragma once
 #include <cctype>  //For isdigit and isalpha
 
+//testing table-driven approach
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <iterator>
+#include <unordered_map>
+
 #include "QNode.h"
 #include "QueryValidator.h"
+
+//using namespace std;
 
 QueryValidator::QueryValidator() {
 }
 QueryValidator::~QueryValidator() {
 }
+
+// Initalise table for flexibility in changing program design entity relationships
+//Currently, Visual studio C++ dont support  initializer lists. Thinking of another workaround. 
+/*void QueryValidator::initTable()
+{
+	vector<string> list1 {"stmt","assign","while","prog_line","string-int"};  
+	relationshipArg1Map.insert(make_pair("Modifies", list1));
+	vector<string> list2 {"variable","string-char","_"};
+	relationshipArg2Map.insert(make_pair("Modifies", list2));
+
+	vector<string> list3 {"stmt","assign","while","prog_line","string-int"};  
+	relationshipArg1Map.insert(make_pair("Uses", list3));
+	vector<string> list4 {"variable","string-char","_"};
+	relationshipArg2Map.insert(make_pair("Uses", list4));
+}*/
 
 /**
 * Method to validate the arguments of all such that clause
@@ -17,6 +42,54 @@ QueryValidator::~QueryValidator() {
 */
 bool QueryValidator::validateSuchThatQueries(QNODE_TYPE type, Synonym arg1, Synonym arg2)
 {	
+	//testing code to move towards table-driven method
+	/*vector<string> listArg1 = relationshipArg1Map.at(type);
+	vector<string> listArg2 = relationshipArg2Map.at(type);
+	string arg1Type = arg1.getType();
+	string arg2Type = arg2.getType();
+	if (arg1Type == "String") 
+	{
+		char arg1Value = arg1.getName()[0];  //Get the value of arg1
+		if (isdigit(arg1Value)) 
+		{
+			arg1Type == "string-int";
+		}
+		else if (isalpha(arg1Value)) 
+		{
+			arg1Type == "string-char";
+		}
+		else
+		{
+			arg1Type == "string-mix";	
+		}
+	}
+	if (arg2Type == "String") 
+	{
+		char arg2Value = arg2.getName()[0];  //Get the value of arg1
+		if (isdigit(arg2Value)) 
+		{
+			arg2Type == "string-int";
+		}
+		else if (isalpha(arg2Value)) 
+		{
+			arg2Type == "string-char";
+		}
+		else
+		{
+			arg2Type == "string-mix";	
+		}
+	}
+
+	auto result1 = std::find(std::begin(listArg1), std::end(listArg1), arg1Type);
+	auto result2 = std::find(std::begin(listArg2), std::end(listArg2), arg2Type);
+	if(result1 == std::end(listArg1)) // not inside list of type of argument 1
+		return false;
+	if(result2 == std::end(listArg2)) // not inside list of type of argument 2
+		return false;
+	if(arg1Type!="string-int" && arg2Type!="string-int" && arg1.getName() == arg2.getName())
+		return false; //arg1 and arg2 cannot have the same names if they are synoyms
+	*/
+
 	switch (type) 
 	{
 		case Modifies:
--- source/QueryValidator.h
+++ source/QueryValidator.h
@@ -6,10 +6,14 @@ class QueryValidator {
 public:
 	 QueryValidator();
 	 ~ QueryValidator();
+	//void initTable();
 	bool validateSuchThatQueries(QNODE_TYPE type, Synonym arg1, Synonym arg2);
+
 protected:
 	bool validateModifiesOrUsesArgs(Synonym arg1, Synonym arg2);//Validates the inputs for modifies and uses
 	bool validateParentArgs(Synonym arg1, Synonym arg2);//Validates the inputs for parent
 	bool validateFollowsArgs(Synonym arg1, Synonym arg2);//Validates the inputs for follows
 
+	//unordered_map<QNODE_TYPE, vector<string>> relationshipArg1Map; //maps the relationship to its type of argument 1
+	//unordered_map<QNODE_TYPE, vector<string>> relationshipArg2Map; //maps the relationship to its type of argument 2
 };
