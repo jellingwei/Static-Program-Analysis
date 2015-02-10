423b1b8579e94119474d0ee310f1365bc40d5a34
Kang HJ <kanghongjin@gmail.com>
2015-02-10 16:03:05 +0800

    Test cases for statements when there are multiple procedures

--- IntegrationTesting/source/TestPKB.cpp
+++ IntegrationTesting/source/TestPKB.cpp
@@ -71,6 +71,7 @@ void PKBTest::testPKB()
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Parent(1, 2)", false, pkb.isParent(1,2));
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Parent(1, 2)", false, pkb.isParent(1,2));
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Parent(0, 1)", false, pkb.isParent(0,1));
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Parent(21, 25)", false, pkb.isParent(21, 25));
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Parent(13, 15)", true, pkb.isParent(13,15));
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Parent(9999, 10000)", false, pkb.isParent(9999,10000));
 	
@@ -97,6 +98,7 @@ void PKBTest::testPKB()
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Parent*(7, 9)", true, pkb.isParentS(7, 9));
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Parent*(12, 13)", true, pkb.isParentS(12, 13));
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Parent*(12, 14)", true, pkb.isParentS(12, 14));
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Parent*(21, 25)", true, pkb.isParentS(21, 25));
 	
 
 	// Follows
@@ -109,6 +111,7 @@ void PKBTest::testPKB()
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Follows(8, 9)", false, pkb.isFollows(8,9));
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Follows(8, 10)", true, pkb.isFollows(8,10));
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Follows(7, 10)", false, pkb.isFollows(7,10));
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Follows(17, 18)", true, pkb.isFollows(17,18));
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Follows(s, 6)", 5, pkb.getStmtFollowedTo(6).front());
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Follows(s, 10)", 8, pkb.getStmtFollowedTo(10).front());
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Follows(s, 1)", 0, (int)pkb.getStmtFollowedTo(1).size());
@@ -146,14 +149,24 @@ void PKBTest::testPKB()
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Follows*(0, 12)", false, pkb.isFollowsS(0, 12));
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Follows*(13, 14)", false, pkb.isFollowsS(13, 14));
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Follows*(14, 15)", false, pkb.isFollowsS(14, 15));
-
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Follows*(17, 18)", true, pkb.isFollowsS(17, 18));
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Follows*(17, 19)", false, pkb.isFollowsS(17, 19));
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Follows*(17, 21)", false, pkb.isFollowsS(17, 21));
 
 	// Uses
 	cout << "Uses" << endl;
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Uses(2, 'd')", true, pkb.isUses(2, pkb.getVarIndex("d")));
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Uses(2, 'f')", true, pkb.isUses(2, pkb.getVarIndex("f")));
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Uses(12, 'a')", true, pkb.isUses(12, pkb.getVarIndex("a")));
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Uses(12, 'b')", true, pkb.isUses(12, pkb.getVarIndex("b")));
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Uses(15, 'a')", true, pkb.isUses(15, pkb.getVarIndex("a")));
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Uses(2, 'a')", false, pkb.isUses(2, pkb.getVarIndex("a")));
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Uses(17, 'c')", true, pkb.isUses(17, pkb.getVarIndex("c")));
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Uses(18, 'a')", true, pkb.isUses(18, pkb.getVarIndex("a")));
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Uses(18, 'c')", true, pkb.isUses(18, pkb.getVarIndex("c")));
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Uses(26, 'a')", true, pkb.isUses(26, pkb.getVarIndex("a")));
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Uses(26, 'c')", true, pkb.isUses(26, pkb.getVarIndex("c")));
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Uses(26, 'd')", false, pkb.isUses(26, pkb.getVarIndex("d")));
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Uses(3, v)", (string)"a", (string)pkb.getVarName(pkb.getUsesVarForStmt(3).front())); 
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Uses(7, v)", 4, (int)pkb.getUsesVarForStmt(7).size());
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Uses(11, v)", 3, (int)pkb.getUsesVarForStmt(11).size()); 
@@ -161,6 +174,7 @@ void PKBTest::testPKB()
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Uses(s, 'c')", 8, pkb.getUsesStmtNum(pkb.getVarIndex("c")).front());
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Uses(s, 'f')", 5, (int)pkb.getUsesStmtNum(pkb.getVarIndex("f")).size());
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Uses(1, _)", 0, (int)pkb.getUsesVarForStmt(1).size());
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Uses(20, _)", 3, (int)pkb.getUsesVarForStmt(20).size());
 
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Uses for procIndex 0", 6, (int)pkb.getUsesVarForProc(0).size());
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Uses for procIndex 1", 3, (int)pkb.getUsesVarForProc(1).size());
@@ -174,10 +188,15 @@ void PKBTest::testPKB()
 	// Modifies
 	cout << "Modifies" << endl;
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Modifies(1, 'a')", true, pkb.isModifies(1, pkb.getVarIndex("a")));
-	CPPUNIT_ASSERT_EQUAL_MESSAGE("Modifies(15, 'a')", true, pkb.isModifies(15, pkb.getVarIndex("a")));
-	CPPUNIT_ASSERT_EQUAL_MESSAGE("Modifies(7, 'f')", true, pkb.isModifies(7, pkb.getVarIndex("f")));
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Modifies(5, 'e')", false, pkb.isModifies(5, pkb.getVarIndex("e")));
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Modifies(7, 'b')", false, pkb.isModifies(7, pkb.getVarIndex("b")));
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Modifies(7, 'f')", true, pkb.isModifies(7, pkb.getVarIndex("f")));
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Modifies(15, 'a')", true, pkb.isModifies(15, pkb.getVarIndex("a")));
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Modifies(17, 'a')", true, pkb.isModifies(17, pkb.getVarIndex("a")));
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Modifies(20, 'b')", false, pkb.isModifies(20, pkb.getVarIndex("b")));
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Modifies(20, 'a')", true, pkb.isModifies(20, pkb.getVarIndex("a")));
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Modifies(26, 'b')", false, pkb.isModifies(26, pkb.getVarIndex("b")));
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Modifies(26, 'a')", true, pkb.isModifies(26, pkb.getVarIndex("a")));
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Modifies(s, 'd')", 5, pkb.getModStmtNum(pkb.getVarIndex("d")).front());
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Modifies(s, 'b')", 2, (int)pkb.getModStmtNum(pkb.getVarIndex("b")).size());
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Modifies(7, v)", 2, (int)pkb.getModVarForStmt(7).size());
