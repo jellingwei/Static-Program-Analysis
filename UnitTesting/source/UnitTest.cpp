ef6cb68e3d68af2894ef3c9060880354b2588bf8
User <tongchunkit@gmail.com>
2015-03-18 14:08:35 +0800

    Changed intermediate values handler to use hash join. Surprisingly, there is not much difference between that and the old method.

--- UnitTesting/source/UnitTest.cpp
+++ UnitTesting/source/UnitTest.cpp
@@ -6,30 +6,29 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-// Get the top level suite from the registry
-CppUnit::TestSuite *unitSuite = new CppUnit::TestSuite( "All unit test" );
-unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("VarTableTest").makeTest());
-unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("ProcTableTest").makeTest());
-unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("ConstantTableTest").makeTest());	
-unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("CallsTableTest").makeTest());
-unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("UsesTableTest").makeTest());	
-unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("ModifiesTableTest").makeTest());	
-unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("ParserTest").makeTest());
-unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("ExpressionParserTest").makeTest());
-unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("QueryPreprocessorTest").makeTest());
-unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("ProcTableTest").makeTest());
-unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("QueryTreeTest").makeTest());
-unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("IntermediateValuesHandlerTest").makeTest());
-unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("ASTTest").makeTest());
-unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("PatternMatchTest").makeTest());
-CppUnit::TestFactoryRegistry::getRegistry().addTestToSuite(unitSuite);
-CppUnit::TextUi::TestRunner runner;
+	// Get the top level suite from the registry
+	CppUnit::TestSuite *unitSuite = new CppUnit::TestSuite( "All unit test" );
+	unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("VarTableTest").makeTest());
+	unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("ProcTableTest").makeTest());
+	unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("ConstantTableTest").makeTest());	
+	unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("CallsTableTest").makeTest());
+	unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("UsesTableTest").makeTest());	
+	unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("ModifiesTableTest").makeTest());	
+	unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("ParserTest").makeTest());
+	unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("ExpressionParserTest").makeTest());
+	unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("QueryPreprocessorTest").makeTest());
+	unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("ProcTableTest").makeTest());
+	unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("QueryTreeTest").makeTest());
+	//unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("IntermediateValuesHandlerTest").makeTest());
+	unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("ValuesHandlerTest").makeTest());
+	unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("ASTTest").makeTest());
+	unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("PatternMatchTest").makeTest());
+	CppUnit::TestFactoryRegistry::getRegistry().addTestToSuite(unitSuite);
+	CppUnit::TextUi::TestRunner runner;
 
-
-runner.addTest(unitSuite);
-bool wasSucessful = runner.run();
-
-//getchar();
-
-return wasSucessful ? 0 : 1;
+	runner.addTest(unitSuite);
+	bool wasSucessful = runner.run();
+	//getchar();
+	system("pause");
+	return wasSucessful ? 0 : 1;
 }
---
+++ source/ValuesHandler.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <vector>
+#include <set>
+#include <unordered_map>
+using std::unordered_map;
+using std::unordered_multimap;
+using std::set;
+using std::vector;
+using std::pair;
+using std::string;
+using std::swap;
+
+#include "Synonym.h"
+#include "PKB.h"
+
+#define MAX_SYNONYMS_ASSUMED 100
+
+namespace ValuesHandler 
+{
+	//Public functions to be used
+	void initialize(unordered_map<string, SYNONYM_TYPE> synonymMap);
+	bool addAndProcessIntermediateSynonym(Synonym synonym);
+	bool addAndProcessIntermediateSynonyms(Synonym LHS, Synonym RHS);
+	
+	Synonym getSynonym(string wantedName);
+	bool filterEqualValue(Synonym synonym, string wantedValue);
+	bool filterEqualPair(Synonym LHS, Synonym RHS);
+	bool isValueExistInSet(set<int> setToSearch, int value);
+	vector<int> getDefaultValues(SYNONYM_TYPE type);
+	set<int> getIntermediateValuesSet(string wantedName);
+	vector<int> getIntermediateValues(string wantedName);
+
+	//Functions made public for testing purposes
+	void clearAll();
+	int findIndexInMainTable(string synonymName);
+	bool isExistInMainTable(string synonymName);
+	bool isExistInSingletonTable(string synonymName);
+	bool addToSingletonTableForTesting(Synonym synonym);
+	inline void removeFromSingletonTable(string synonymName);
+	set<int> getIntermediateValuesSetInMain(int synonymIndex);
+	vector<int> getIntermediateValuesInMain(int synonymIndex);
+
+	bool hashIntersectWithMainTable(Synonym synonym);
+	bool hashIntersectWithMainTable(Synonym LHS, Synonym RHS);
+	bool hashJoinWithMainTable(Synonym mainSynonym, Synonym pairedSynonym);
+	bool joinWithMainTable(Synonym LHS, Synonym RHS);
+
+	pair<vector<int>, vector<int>> getPairBySingletonIntersect(Synonym singleton, Synonym singletonPair);
+	pair<vector<int>, vector<int>> getPairBySingletonStringIntersect(Synonym LHS, Synonym RHS);
+	bool hashIntersectWithSingletonTable(Synonym synonym);
+
+	bool processPairWithMainTable(Synonym LHS, Synonym RHS);
+	bool processPairWithSingletonTable(Synonym LHS, Synonym RHS);
+	bool mergeSingletonToMain(Synonym mainSynonym, Synonym singleton);
+
+}
