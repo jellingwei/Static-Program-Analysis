9f2ce4caa1ae247d57bd7977dd676d5b0a9221a5
User <tongchunkit@gmail.com>
2015-03-29 14:32:32 +0800

    Added VALUE_LIST to represent vector<int>

--- source/common.h
+++ source/common.h
@@ -33,6 +33,7 @@ typedef string EXPRESSION;
 typedef string STATEMENT_TYPE;
 
 typedef set<int> VALUE_SET;
+typedef vector<int> VALUE_LIST;
 typedef vector<int> CONSTANT_LIST;
 typedef vector<int> STATEMENT_LIST;
 typedef vector<int> PROGLINE_LIST;
