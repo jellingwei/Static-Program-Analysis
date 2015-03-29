7ae90fbc1f10e6892dfdf7985cfdd292af3b2f23
Si Ling <siling1992@gmail.com>
2015-03-29 13:40:51 +0800

    edits to common.h

--- source/common_list.h
+++ source/common_list.h
@@ -7,5 +7,3 @@
 typedef vector<Synonym> SYNONYM_LIST;
 typedef vector<TNode*>* TNODE_LIST;
 typedef vector<CNode*>* CNODE_LIST;
-typedef vector<VAR_INDEX> VARINDEX_LIST;
-typedef vector<PROC_INDEX> PROCINDEX_LIST;
